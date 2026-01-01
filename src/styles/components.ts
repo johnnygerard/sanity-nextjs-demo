6e72a26844857c225e0bf00d3f67426e2b9ea0b7
Johnny Gérard <john@jgerard.dev>
2026-01-01 14:37:07 +0100

    feat: set default focus ring styles

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -5,5 +5,4 @@ export const buttonStyles = tw([
   "bg-gray-900 px-4 py-2",
   "text-sm font-medium text-gray-50",
   "transition-colors hover:bg-gray-800",
-  "outline-offset-2 focus-visible:outline-gray-900/40",
 ]);
